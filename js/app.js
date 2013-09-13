58fa52f56e56f6c3f892b9be3a2877231309a1f1
Tri Nguyen <tri@tridnguyen.com>
2013-09-13 00:11:56 -0400

    footer and link styles

--- js/app.js
+++ js/app.js
@@ -3,8 +3,6 @@ define(function (require) {
 	var config = require('json!config.json'),
 		$ = require('jquery');
 
-	console.log(config);
-
 	$(document).ready(function(){
 		$('#contact input[type="submit"]').click(function(){
 			return false;
