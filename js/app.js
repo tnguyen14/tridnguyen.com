258840c1b40b1f9fe4ffc868c92eb17e7a922011
Tri Nguyen <tri@tridnguyen.com>
2013-09-25 01:10:51 -0400

    submit contact form

--- js/app.js
+++ js/app.js
@@ -7,6 +7,21 @@ define(function (require) {
 	$(document).ready(function(){
 		Prism.highlightAll();
 		$('#contact input[type="submit"]').click(function(){
+			var $form = $('#contact');
+			$.ajax({
+				url: "http://localhost:3000/forms/2",
+				data: $form.serialize(),
+				type: "POST",
+				success: function(data, status){
+					console.log(data);
+					$form.html('Thank you! Your message has been received.');
+				},
+				error: function(error) {
+					if (error) {
+						console.log(error);
+					}
+				}
+			});
 			return false;
 		});
 	});
