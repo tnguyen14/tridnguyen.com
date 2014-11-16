5464d2dd2eeb623105b25ff32d7cb317737b5394
Tri Nguyen <tri@tridnguyen.com>
2014-11-15 23:03:48 -0500

    wrap template helpers with umd

--- templates/helpers/getDate.js
+++ templates/helpers/getDate.js
@@ -3,28 +3,46 @@
 // @param {String|Object} a date string (http://momentjs.com/docs/#/parsing/string/) or momentjs date object
 // @return {String} the date string in the desired format
 
-var  moment = require('moment');
-
-module.exports = function(formatStr, date) {
-	var momentDate;
-	if (!date) {
-		throw new Error('Invalid date param.');
-	}
-	if ((date && date.data)) {
-		momentDate = moment();
+(function (root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module.
+		define(['components/moment/moment'], factory);
+	} else if (typeof exports === 'object') {
+		// Node. Does not work with strict CommonJS, but
+		// only CommonJS-like environments that support module.exports,
+		// like Node.
+		module.exports = factory(require('moment'));
 	} else {
-		// if the date passed in is not a moment type, convert it to moment
-		// otherwise use it
-		if (!moment.isMoment(date)) {
-			momentDate = moment(date);
-			// if the date parsing fails
-			if (!momentDate.isValid()) {
-				throw new Error('The date passed in is invalid');
-			}
-		} else {
-			momentDate = date;
-		}
+		// Browser globals (root is window)
+		root.returnExports = factory(root.moment);
 	}
+}(this, function (moment) {
+	//use b in some fashion.
 
-	return momentDate.format(formatStr)
-}
+	// Just return a value to define the module export.
+	// This example returns an object, but the module
+	// can return a function as the exported value.
+	return function (formatStr, date) {
+		var momentDate;
+		if (!date) {
+			throw new Error('Invalid date param.');
+		}
+		if ((date && date.data)) {
+			momentDate = moment();
+		} else {
+			// if the date passed in is not a moment type, convert it to moment
+			// otherwise use it
+			if (!moment.isMoment(date)) {
+				momentDate = moment(date);
+				// if the date parsing fails
+				if (!momentDate.isValid()) {
+					throw new Error('The date passed in is invalid');
+				}
+			} else {
+				momentDate = date;
+			}
+		}
+
+		return momentDate.format(formatStr);
+	};
+}));
--- templates/helpers/isEqual.js
+++ templates/helpers/isEqual.js
@@ -1,7 +1,26 @@
-module.exports = function(a, b, options) {
-	if (a === b) {
-		return options.fn(this);
+(function (root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module.
+		define([], factory);
+	} else if (typeof exports === 'object') {
+		// Node. Does not work with strict CommonJS, but
+		// only CommonJS-like environments that support module.exports,
+		// like Node.
+		module.exports = factory();
 	} else {
-		return options.inverse(this);
-	}
-}
+		// Browser globals (root is window)
+		root.returnExports = factory();
+  }
+}(this, function () {
+
+	// Just return a value to define the module export.
+	// This example returns an object, but the module
+	// can return a function as the exported value.
+	return function (a, b, options) {
+		if (a === b) {
+			return options.fn(this);
+		} else {
+			return options.inverse(this);
+		}
+	};
+}));
