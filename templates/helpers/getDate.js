58db2d5517388a78899d35681c9a3ed59e1a69f8
Tri Nguyen <tri@tridnguyen.com>
2014-11-16 01:26:55 -0500

    register helper right in their declaration

--- templates/helpers/getDate.js
+++ templates/helpers/getDate.js
@@ -6,23 +6,19 @@
 (function (root, factory) {
 	if (typeof define === 'function' && define.amd) {
 		// AMD. Register as an anonymous module.
-		define(['components/moment/moment'], factory);
+		define(['hbs/handlebars', 'moment'], factory);
 	} else if (typeof exports === 'object') {
 		// Node. Does not work with strict CommonJS, but
 		// only CommonJS-like environments that support module.exports,
 		// like Node.
-		module.exports = factory(require('moment'));
+		module.exports = factory(require('handlebars'), require('moment'));
 	} else {
 		// Browser globals (root is window)
 		root.returnExports = factory(root.moment);
 	}
-}(this, function (moment) {
-	//use b in some fashion.
-
-	// Just return a value to define the module export.
-	// This example returns an object, but the module
-	// can return a function as the exported value.
-	return function (formatStr, date) {
+}(this, function (Handlebars, moment) {
+	console.log(arguments);
+	function getDate (formatStr, date) {
 		var momentDate;
 		if (!date) {
 			throw new Error('Invalid date param.');
@@ -30,6 +26,7 @@
 		if ((date && date.data)) {
 			momentDate = moment();
 		} else {
+			console.log(moment);
 			// if the date passed in is not a moment type, convert it to moment
 			// otherwise use it
 			if (!moment.isMoment(date)) {
@@ -45,4 +42,6 @@
 
 		return momentDate.format(formatStr);
 	};
+	Handlebars.registerHelper('getDate', getDate);
+	return getDate;
 }));
--- templates/helpers/step.js
+++ templates/helpers/step.js
@@ -4,22 +4,22 @@
 (function (root, factory) {
 	if (typeof define === 'function' && define.amd) {
 		// AMD. Register as an anonymous module.
-		define([], factory);
+		define(['hbs/handlebars'], factory);
 	} else if (typeof exports === 'object') {
 		// Node. Does not work with strict CommonJS, but
 		// only CommonJS-like environments that support module.exports,
 		// like Node.
-		module.exports = factory();
+		module.exports = factory(require('handlebars'));
 	} else {
 		// Browser globals (root is window)
 		root.returnExports = factory();
   }
-}(this, function () {
+}(this, function (Handlebars) {
 
 	// Just return a value to define the module export.
 	// This example returns an object, but the module
 	// can return a function as the exported value.
-	return function(steps, options) {
+	function step (steps, options) {
 		for (var key in this) {
 			if (this.hasOwnProperty(key)) {
 				if (steps === 1) {
@@ -30,4 +30,6 @@
 			}
 		}
 	};
+	Handlebars.registerHelper('step', step);
+	return step;
 }));
