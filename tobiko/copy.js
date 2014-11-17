e29aaef1bd3bef69c80109ddcbabd85ef25501cf
Tri Nguyen <tri@tridnguyen.com>
2014-11-17 00:21:26 -0500

    copy all images by default, fix #19

--- tobiko/copy.js
+++ tobiko/copy.js
@@ -4,7 +4,9 @@ module.exports = {
 			{expand: true, src: ['CNAME'], dest: '<%= buildPath %>/'},
 			{expand: true, cwd: 'components', src: ['fancybox/source/**/*'], dest: '<%= buildPath %>/components'},
 			{expand: true, cwd: 'sass', src: 'assets/**/*', dest: '<%= buildPath %>/css/'},
-			{expand: true, cwd: 'js/lib', src: '**/*', dest: '<%= buildPath %>/js/lib'}
+			{expand: true, cwd: 'js/lib', src: '**/*', dest: '<%= buildPath %>/js/lib'},
+			// all images by default
+			{expand: true, cwd: 'contents', src: '**/*.{jpg,png,svg}', dest: '<%= buildPath %>'}
 		]
 	}
 }
