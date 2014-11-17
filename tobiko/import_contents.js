b480201ec0f338e5c125dd950acab478b1b1daa3
Tri Nguyen <tri@tridnguyen.com>
2014-11-16 23:27:17 -0500

    pagination

--- tobiko/import_contents.js
+++ tobiko/import_contents.js
@@ -8,15 +8,7 @@ module.exports = {
 			smartLists: true,
 			smartypants: true,
 			langPrefix: 'language-'
-		},
-		paginate: [
-			{
-				dir: 'articles',
-				postPerPage: 4,
-				template: 'archive.hbs',
-				title: 'Articles'
-			}
-		]
+		}
 	},
 	all: {
 		src: 'contents/**/*.{json,md}',
