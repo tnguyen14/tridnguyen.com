a096a3a973cd2150e0df097481ae981090e4e024
Tri Nguyen <tri@tridnguyen.com>
2015-05-17 01:42:08 -0400

    clean up javascript

--- js/app.js
+++ js/app.js
@@ -1,81 +1,21 @@
+/* global define, page */
 define(function (require) {
-
-	var config = require('json!config.json'),
-		configDev = require('json!config-dev.json'),
-		$ = require('jquery'),
-		_ = require('lodash');
+	'use strict';
+	var config = require('json!config.json');
+	var configDev = require('json!config-dev.json');
+	var $ = require('jquery');
+	var _ = require('lodash');
 
 	require('validation');
 	require('bxslider');
 	require('fancybox');
 
-	var articleTemplate = require('hbs!templates/partials/article'),
-		feedTemplate = require('hbs!templates/partials/feed'),
-		loader = require('hbs!templates/partials/loader');
-
 	if (page.env === 'dev') {
 		_.extend(config, configDev);
 	}
 
-	function getParameterByName(name) {
-		name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-		var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-			results = regex.exec(location.search);
-		return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-	}
-
-	var currentPage = +getParameterByName('page') || 1;
-
-	// get all the posts for the current page
-	// baed on posts_per_page
-	var getPosts = function () {
-		var $mainContent = $('.main-content');
-		$mainContent.html(loader);
-		$.ajax(config.apiRoot + '/posts', {
-			data: {
-				filter: {
-					posts_per_page: getParameterByName('posts_per_page') || 5
-				},
-				page: currentPage
-			},
-			success: function (data) {
-				$mainContent.empty().html(feedTemplate({
-					posts: data,
-					prevPage: (data.length === 0) ? undefined : currentPage + 1, // if receive no more posts, do not go back any further
-					nextPage: (currentPage === 1) ? undefined : currentPage - 1
-				}));
-				Prism.highlightAll();
-			}
-		});
-	}
-
-	var getPost = function (slug) {
-		if (!slug) { return; }
-		var $mainContent = $('.main-content');
-		$mainContent.html(loader);
-		$.ajax(config.apiRoot + '/posts', {
-			data: {
-				filter: {
-					name: slug
-				}
-			},
-			success: function (data) {
-				$mainContent.empty().html(articleTemplate(data[0]));
-				Prism.highlightAll();
-			}
-		});
-	}
-
 	$(document).ready(function () {
-		switch (page.context) {
-			case 'main':
-				getPosts();
-				break;
-			case 'post':
-				getPost(page.slug);
-				break;
-		}
-		$("#contact input[type='submit']").on('click', function (e) {
+		$('#contact input[type="submit"]').on('click', function (e) {
 			e.preventDefault();
 			var $form = $('#contact');
 			$form.validate({
@@ -95,14 +35,12 @@ define(function (require) {
 					url: 'http://inspired-forms.herokuapp.com/forms/52e49f41dd1cae0200000002',
 					data: $form.serialize(),
 					type: 'POST',
-					success: function(data, status){
-						console.log(data);
+					success: function () {
 						$form.html('<p>Thank you! Your message has been received. I will be in touch shortly.</p>');
 					},
 					error: function(error) {
 						if (error) {
 							$(e.target).prop('disabled', false);
-							console.log(error);
 						}
 					}
 				});
