fbe155cb47d722af0f7627a922e6f9f382176aeb
Tri Nguyen <tri@tridnguyen.com>
2015-05-17 01:41:31 -0400

    add wordpress config

--- tobiko/import_contents.js
+++ tobiko/import_contents.js
@@ -8,10 +8,25 @@ module.exports = {
 			smartLists: true,
 			smartypants: true,
 			langPrefix: 'language-'
+		},
+		wordpress: {
+			apiRoot: 'http://wp.tridnguyen.com/wp-json',
+			contents: [{
+				postType: 'posts',
+				folder: 'articles',
+				template: 'article.hbs'
+			}]
+		},
+		archives: {
+			articles: {
+				postsPerPage: 4,
+				title: 'Articles',
+				template: 'articles.hbs'
+			}
 		}
 	},
 	all: {
 		src: 'contents/**/*.{json,md}',
 		dest: 'build/data.json'
 	}
-}
+};
