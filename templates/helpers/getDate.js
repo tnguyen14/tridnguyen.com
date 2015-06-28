71771287c37b6a75d7497fe476db51ffe2406a2f
Tri Nguyen <tri@tridnguyen.com>
2015-06-28 15:59:05 -0400

    remove umd shims

--- templates/helpers/getDate.js
+++ templates/helpers/getDate.js
@@ -1,45 +1,33 @@
+'use strict';
+
+var Handlebars = require('handlebars');
+var moment = require('moment');
+
 // Display date
 // @param {String} format of the desired output, see momentjs date format http://momentjs.com/docs/#/displaying/format/
 // @param {String|Object} a date string (http://momentjs.com/docs/#/parsing/string/) or momentjs date object
 // @return {String} the date string in the desired format
-
-(function (root, factory) {
-	if (typeof define === 'function' && define.amd) {
-		// AMD. Register as an anonymous module.
-		define(['hbs/handlebars', 'moment'], factory);
-	} else if (typeof exports === 'object') {
-		// Node. Does not work with strict CommonJS, but
-		// only CommonJS-like environments that support module.exports,
-		// like Node.
-		module.exports = factory(require('handlebars'), require('moment'));
-	} else {
-		// Browser globals (root is window)
-		root.returnExports = factory(root.moment);
+function getDate (formatStr, date) {
+	var momentDate;
+	if (!date) {
+		throw new Error('Invalid date param.');
 	}
-}(this, function (Handlebars, moment) {
-	function getDate (formatStr, date) {
-		var momentDate;
-		if (!date) {
-			throw new Error('Invalid date param.');
-		}
-		if ((date && date.data)) {
-			momentDate = moment();
-		} else {
-			// if the date passed in is not a moment type, convert it to moment
-			// otherwise use it
-			if (!moment.isMoment(date)) {
-				momentDate = moment(date);
-				// if the date parsing fails
-				if (!momentDate.isValid()) {
-					throw new Error('The date passed in is invalid');
-				}
-			} else {
-				momentDate = date;
+	if ((date && date.data)) {
+		momentDate = moment();
+	} else {
+		// if the date passed in is not a moment type, convert it to moment
+		// otherwise use it
+		if (!moment.isMoment(date)) {
+			momentDate = moment(date);
+			// if the date parsing fails
+			if (!momentDate.isValid()) {
+				throw new Error('The date passed in is invalid');
 			}
+		} else {
+			momentDate = date;
 		}
+	}
 
-		return momentDate.format(formatStr);
-	};
-	Handlebars.registerHelper('getDate', getDate);
-	return getDate;
-}));
+	return momentDate.format(formatStr);
+}
+Handlebars.registerHelper('getDate', getDate);
--- templates/helpers/step.js
+++ templates/helpers/step.js
@@ -1,35 +1,19 @@
+'use strict';
+
+var Handlebars = require('handlebars');
+
 // step into an object
-// @param: steps equals the number of levels to go depeer in the object
-
-(function (root, factory) {
-	if (typeof define === 'function' && define.amd) {
-		// AMD. Register as an anonymous module.
-		define(['hbs/handlebars'], factory);
-	} else if (typeof exports === 'object') {
-		// Node. Does not work with strict CommonJS, but
-		// only CommonJS-like environments that support module.exports,
-		// like Node.
-		module.exports = factory(require('handlebars'));
-	} else {
-		// Browser globals (root is window)
-		root.returnExports = factory();
-  }
-}(this, function (Handlebars) {
-
-	// Just return a value to define the module export.
-	// This example returns an object, but the module
-	// can return a function as the exported value.
-	function step (steps, options) {
-		for (var key in this) {
-			if (this.hasOwnProperty(key)) {
-				if (steps === 1) {
-					return options.fn(this[key]);
-				} else {
-					step(steps-1);
-				}
+// @param steps equals the number of levels to go depeer in the object
+function step (steps, options) {
+	/* jshint validthis:true */
+	for (var key in this) {
+		if (this.hasOwnProperty(key)) {
+			if (steps === 1) {
+				return options.fn(this[key]);
+			} else {
+				step(steps-1);
 			}
 		}
-	};
-	Handlebars.registerHelper('step', step);
-	return step;
-}));
+	}
+}
+Handlebars.registerHelper('step', step);
